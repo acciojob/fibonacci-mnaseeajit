151d09ed3502684569b0d5511958df788ec79cc5
mnaseeajit <147361506+mnaseeajit@users.noreply.github.com>
2023-12-09 13:41:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-// your code here
+    if (num == 1) {
+        return 0;
+    }
+    if (num == 2 ) {
+        return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-module.exports = fibonacci;
+let value = prompt("Enter value");
+value = parseInt(value); // Convert input to a number
+alert(fibonacci(value));
+           
+ 
